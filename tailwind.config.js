d7765255ea229ef2fc80f0de9a3df078115482e3
shaakilahammed <shaakilahammed@gmail.com>
2024-05-21 14:58:01 +0600

    setup: next, tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        screen: {
+            sm: '576px',
+            md: '768px',
+            lg: '992px',
+            xl: '1200px',
+        },
+        container: {
+            center: true,
+            padding: '1rem',
+        },
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+                roboto: ['Roboto', 'sans-serif'],
+            },
+            colors: {
+                primary: '#fd3d57',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [require('@tailwindcss/forms')],
 };
