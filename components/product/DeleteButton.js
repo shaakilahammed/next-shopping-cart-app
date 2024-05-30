acde8c926d5566a5c92a576551098436ee89a42b
shaakilahammed <shaakilahammed@gmail.com>
2024-05-30 18:35:24 +0600

    fix

--- components/product/DeleteButton.js
+++ components/product/DeleteButton.js
@@ -1,22 +1,43 @@
 'use client';
-import { removeFromWishlist } from '@/actions/wish-list';
+import { getBaseUrl } from '@/utils/utils';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRouter } from 'next/navigation';
 
 const DeleteButton = ({ accessToken, productId, colorId }) => {
     const router = useRouter();
-    const handleClick = async () => {
-        await removeFromWishlist(accessToken, productId, colorId);
-        router.refresh();
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${getBaseUrl()}/api/wish-list`, {
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ productId, colorId }),
+            });
+
+            const data = await response.json();
+            console.log(data);
+            if (response.ok) {
+                return;
+            } else {
+                router.push('/login');
+            }
+        } catch (error) {
+            router.push('/login');
+        }
     };
     return (
-        <button
-            onClick={handleClick}
-            className="text-gray-600 cursor-pointer hover:text-primary"
-        >
-            <FontAwesomeIcon icon={faTrash} />
-        </button>
+        <form onSubmit={submitHandler}>
+            <button
+                type="submit"
+                className="text-gray-600 cursor-pointer hover:text-primary"
+            >
+                <FontAwesomeIcon icon={faTrash} />
+            </button>
+        </form>
     );
 };
 
